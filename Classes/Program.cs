17f3be0bb09ff8c28db239d3ba7a73d066c413c4
James Krcelich <james.krcelich@sbcglobal.net>
2025-11-01 16:16:00 -0500

    push Program.cs

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,82 @@
-﻿namespace Classes
+﻿using System;
+using System.Collections.Generic;
+
+namespace Classes
 {
-    public class Program
+    class Program
     {
+        
+        public class Car
+        {
+            public Car() {} // Default Constructor
+   
+            // Custom Constructor
+            public Car(int year, string make, string model, bool isDrivable)
+            {
+                Year = year;
+                Make = make;
+                Model = model;
+                IsDrivable = isDrivable;
+            }
+
+            public int Year; // Property		
+            public string Make { get; set; } // Property
+            public string Model { get; set; } // Property
+            public string EngineNoise { get; set; } // Property
+            public string HonkNoise { get; set; } // Property
+            public bool IsDrivable;
+
+            public void MakeEngineNoise(string engineNoise)
+            {
+                EngineNoise = engineNoise;
+            }
+
+            public void MakeHonkNoise(string honkNoise)
+            {
+                HonkNoise = honkNoise;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Create a separate class file called Car
+            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
+            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
+            //The methods should take one string parameter: the respective noise property
+
+            //Now that the Car class is created we can instantiate 3 new cars
+            //Set the properties for each of the cars
+            //Call each of the methods for each car
+            Car myFirstCar = new Car();
+
+            myFirstCar.Year = 1969;
+            myFirstCar.Make = "Chevrolet";
+            myFirstCar.Model = "Camaro";
+            myFirstCar.MakeEngineNoise("Varoom");
+            myFirstCar.MakeHonkNoise("Beep beep");
+            myFirstCar.IsDrivable = false;
+            
+           // Set the properties utilizing the 3 different ways we learned about, one way for each car
+
+            Car mySecondCar = new Car()
+            {
+                Year = 1989,
+                Make = "Pontiac",
+                Model = "Grand Prix",
+                IsDrivable = true
+            };
+            mySecondCar.MakeEngineNoise("Varoom");
+            mySecondCar.MakeHonkNoise("Beep beep");
+
+            Car myThirdCar = new Car(2025, "Toyota", "Corolla", true);
+            myThirdCar.MakeEngineNoise("Varoom");
+            myThirdCar.MakeHonkNoise("Beep beep");
+            
+            Console.WriteLine($"{myFirstCar.Year} {myFirstCar.Make} {myFirstCar.Model}");  
+            Console.WriteLine($"\t Engine noise = {myFirstCar.EngineNoise}, Car Honk Noise = {myFirstCar.HonkNoise}, Is Drivable = {myFirstCar.IsDrivable}");
+            Console.WriteLine($"{mySecondCar.Year} {mySecondCar.Make} {mySecondCar.Model}");  
+            Console.WriteLine($"\t Engine noise = {mySecondCar.EngineNoise}, Car Honk Noise = {mySecondCar.HonkNoise}, Is Drivable = {mySecondCar.IsDrivable}");
+            Console.WriteLine($"{myThirdCar.Year} {myThirdCar.Make} {myThirdCar.Model}");  
+            Console.WriteLine($"\t Engine noise = {myThirdCar.EngineNoise}, Car Honk Noise = {myThirdCar.HonkNoise}, Is Drivable = {myThirdCar.IsDrivable}");
         }
     }
 }
