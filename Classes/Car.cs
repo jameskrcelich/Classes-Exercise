68a8d89a6dc864222d502addcc844fae929f815e
James Krcelich <james.krcelich@sbcglobal.net>
2025-11-06 18:41:36 -0600

    more final changes

--- Classes/Car.cs
+++ Classes/Car.cs
@@ -1,33 +1,33 @@
 namespace Classes;
 
 
-    public class Car
+public class Car
+{
+    public Car() {} // Default Constructor
+            
+    // Custom Constructor
+    public Car(int year, string make, string model, bool isDrivable)
     {
-        public Car() {} // Default Constructor
-            
-        // Custom Constructor
-        public Car(int year, string make, string model, bool isDrivable)
-        {
-            Year = year;
-            Make = make;
-            Model = model;
-            IsDrivable = isDrivable;
-        }
-            
-        public int Year; // Property		
-        public string Make { get; set; } // Property
-        public string Model { get; set; } // Property
-        public string EngineNoise { get; set; } // Property
-        public string HonkNoise { get; set; } // Property
-        public bool IsDrivable;
-
-        public void MakeEngineNoise(string engineNoise)
-        {
-            EngineNoise = engineNoise;
-        }
-
-        public void MakeHonkNoise(string honkNoise)
-        {
-            HonkNoise = honkNoise;
-        }
+        Year = year;
+        Make = make;
+        Model = model;
+        IsDrivable = isDrivable;
     }
+    
+    public int Year; // Property		
+    public string Make { get; set; } // Property
+    public string Model { get; set; } // Property
+    public string EngineNoise { get; set; } // Property
+    public string HonkNoise { get; set; } // Property
+    public bool IsDrivable;
+
+    public void MakeEngineNoise(string engineNoise)
+    {
+        EngineNoise = engineNoise;
+    }
+
+    public void MakeHonkNoise(string honkNoise)
+    {
+        HonkNoise = honkNoise;
+    }
+}
