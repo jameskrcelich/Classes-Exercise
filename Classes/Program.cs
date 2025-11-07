6b8b55920ab09f480d666175383c53e7cda39c30
James Krcelich <james.krcelich@sbcglobal.net>
2025-11-06 18:35:50 -0600

    final changes

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -5,37 +5,6 @@ namespace Classes
 {
     class Program
     {
-        
-        public class Car
-        {
-            public Car() {} // Default Constructor
-   
-            // Custom Constructor
-            public Car(int year, string make, string model, bool isDrivable)
-            {
-                Year = year;
-                Make = make;
-                Model = model;
-                IsDrivable = isDrivable;
-            }
-
-            public int Year; // Property		
-            public string Make { get; set; } // Property
-            public string Model { get; set; } // Property
-            public string EngineNoise { get; set; } // Property
-            public string HonkNoise { get; set; } // Property
-            public bool IsDrivable;
-
-            public void MakeEngineNoise(string engineNoise)
-            {
-                EngineNoise = engineNoise;
-            }
-
-            public void MakeHonkNoise(string honkNoise)
-            {
-                HonkNoise = honkNoise;
-            }
-        }
         static void Main(string[] args)
         {
             //Create a separate class file called Car
